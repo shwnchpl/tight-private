e40a703e3fdd8415c48a76851033d183046c799b
Shawn Chapla <shwnchpl@gmail.com>
2020-04-05 20:49:17 -0400

    Builder WIP
    Will amend/squash.

--- tight/ast/builder.py
+++ tight/ast/builder.py
@@ -17,15 +17,97 @@
 ###############################################################################
 
 
-from . import AstRoot
-from ..cst import CstVisitor, CstRoot
+from . import AstRoot, model
+from ..cst import CstParser, CstRoot, CstVisitor
 
 
 class AstBuilder(CstVisitor):
-    # TODO: Implement.
-    pass
+    def __init__(self):
+        super().__init__()
+        self._packets = {}
+        self._scope = None
+
+    def visitModule(self, ctx: CstParser.ModuleContext) -> AstRoot:
+        mod = model.Module()
+
+        # Visit each packet, building it.
+
+        # Add to packets dict and module.
+
+        return mod
+
+    def visitPacket(self, ctx: CstParser.PacketContext) -> model.Packet:
+        # Create packet. We'll run into an issue here if our parent
+        # isn't around.
+
+        # Set current scope to packet scope.
+        # old_scope = self._scope
+
+        # Visit each statement in the packet, adding to packet.
+
+        # Restore old scope.
+        # self._scope = old_scope
+
+        pass
+
+    def visitCond_exp(self, ctx: CstParser.Cond_expContext):
+        pass
+
+    def visitCond_relation(self, ctx: CstParser.Cond_relationContext):
+        pass
+
+    def visitCond_conjunction(self, ctx: CstParser.Cond_conjunctionContext):
+        pass
+
+    def visitDef_block(self, ctx: CstParser.Def_blockContext):
+        pass
+
+    def visitAlways_statement(self, ctx: CstParser.Always_statementContext):
+        pass
+
+    def visitOptional_statement(
+            self,
+            ctx: CstParser.Optional_statementContext):
+        pass
+
+    def visitVariable_statement(
+            self,
+            ctx: CstParser.Variable_statementContext):
+        pass
+
+    def visitCase_block(self, ctx: CstParser.Case_blockContext):
+        pass
+
+    def visitCase_statement(self, ctx: CstParser.Case_statementContext):
+        pass
+
+    def visitCase_tag(self, ctx: CstParser.Case_tagContext):
+        pass
+
+    def visitOtherwise_statement(
+            self,
+            ctx: CstParser.Otherwise_statementContext):
+        pass
+
+    def visitField_desc(self, ctx: CstParser.Field_descContext):
+        pass
+
+    def visitScalar_type(self, ctx: CstParser.Scalar_typeContext):
+        pass
+
+    def visitCount(self, ctx: CstParser.CountContext):
+        pass
+
+    def visitValue(self, ctx: CstParser.ValueContext):
+        pass
+
+    def visitResolvable(self, ctx: CstParser.ResolvableContext):
+        pass
+
+    def visitUnits(self, ctx: CstParser.UnitsContext):
+        pass
 
 
 def build_ast(tree: CstRoot) -> AstRoot:
-    # TODO: Implement.
-    pass
+    builder = AstBuilder()
+    return builder.visit(tree)
